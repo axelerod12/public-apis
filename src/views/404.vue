12f28298a3befd278d0e232cc580acee2d1cbb0f
Eduard Chirila <37002543+axelerod12@users.noreply.github.com>
2022-02-13 14:21:25 -0500

    Rename generic error component

---
+++ src/views/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>
+      This page could not be found
+    </h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: '404',
+}
+</script>
--- src/views/Error.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section>This page could not be found</section>
-</template>
-
-<script>
-export default {
-  name: 'Error',
-}
-</script>
